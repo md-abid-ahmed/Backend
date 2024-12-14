a73d2d32f1eee24a51c680bfa58fb0828e490e13
JUNAID AHMED <imrankhan16122001@gmail.com>
2024-12-14 15:05:57 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 //  db always try catch and async await
 import dotenv from "dotenv"
-
-// import express from "express"
+import { app } from "./app.js";
 import connextDb from "./db/index.js";
-// const app=express()
-dotenv.config({
-    path:'./env'
+
+connextDb().then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log("Server is running")
+    })
+}).catch((err)=>{
+    console.log("MOONGO DB CONNECTION FAILED!!",err)
 })
-// async function connextDb(){
-// try{
-//    await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//    app.on("error",(error)=>{
-//     console.log("express is not able to talk to db",error)
-//     throw error
-//    })
-//    app.listen(provess.env.PORT,()=>{
-//     console.log("App is listening mere bhai")
-//    })
-
-// }
-// catch(error){
-// throw err
-// }
-// }
-
-// connextDb();
-
-// ;(async()={})()
-
-
-connextDb()
